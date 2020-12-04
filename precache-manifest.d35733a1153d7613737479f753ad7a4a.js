6931e441b1bb084a8a44d17044e4f9ccc7cdaa09
youngminko <ymk0106@gmail.com>
2020-12-04 17:08:23 -0500

    Updates

---
+++ precache-manifest.d35733a1153d7613737479f753ad7a4a.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b7f794f3ab0500d78d4d8a54cb9b1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "260ede7a2e22bad5a62d",
+    "url": "/static/css/main.de46cd04.chunk.css"
+  },
+  {
+    "revision": "cc1b4f59934957db3736",
+    "url": "/static/js/2.575fa6f0.chunk.js"
+  },
+  {
+    "revision": "ca2abe649ff858defa21eaf931086938",
+    "url": "/static/js/2.575fa6f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260ede7a2e22bad5a62d",
+    "url": "/static/js/main.99940f29.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "c541e0bc2c06d2721197a63b1fe14f59",
+    "url": "/static/media/agile.c541e0bc.png"
+  },
+  {
+    "revision": "2a106907f10c48e5016eb0f755a41e1e",
+    "url": "/static/media/api.2a106907.png"
+  },
+  {
+    "revision": "80bc8d48fbb13d202dd4313d84bbfe41",
+    "url": "/static/media/call.80bc8d48.png"
+  },
+  {
+    "revision": "7c0fc78631f9623a35ae9dc6229310ca",
+    "url": "/static/media/extractTool.7c0fc786.png"
+  },
+  {
+    "revision": "a6a36fca4b5a389d0eefbbd3a9c53a7e",
+    "url": "/static/media/facebook.a6a36fca.svg"
+  },
+  {
+    "revision": "2434c415f0a649a103b2cd5cac74994b",
+    "url": "/static/media/front-end.2434c415.png"
+  },
+  {
+    "revision": "4427fedd16df8ab5da0f388e0d94ffe2",
+    "url": "/static/media/github.4427fedd.svg"
+  },
+  {
+    "revision": "304fb3151e3b1f86d0150dc8144fcc3c",
+    "url": "/static/media/instagram.304fb315.svg"
+  },
+  {
+    "revision": "2c20928ddc18c3e2ac70e6f8f38ca825",
+    "url": "/static/media/linkedin.2c20928d.svg"
+  },
+  {
+    "revision": "7fd95fd844a29503d351df6a5f736f69",
+    "url": "/static/media/microblog.7fd95fd8.png"
+  },
+  {
+    "revision": "787ac833cb3be8ea821d7321e6739c84",
+    "url": "/static/media/migrationTool.787ac833.png"
+  },
+  {
+    "revision": "3de924a23049d414ed980323863eb967",
+    "url": "/static/media/myprofile.3de924a2.jpg"
+  },
+  {
+    "revision": "e2905a883d5f95ef0907cc9adf27ea2d",
+    "url": "/static/media/python.e2905a88.svg"
+  },
+  {
+    "revision": "9e188349967014b7f52086a714a73c49",
+    "url": "/static/media/react.9e188349.svg"
+  },
+  {
+    "revision": "7507348af09b90930ab74cfda3b2f943",
+    "url": "/static/media/resume.7507348a.pdf"
+  },
+  {
+    "revision": "96bb44974695e0ca5b7c0642e0b76959",
+    "url": "/static/media/sudoku.96bb4497.jpg"
+  },
+  {
+    "revision": "79d7bfdf655edd752669d9ba4354dec5",
+    "url": "/static/media/todoList.79d7bfdf.png"
+  }
+]);
